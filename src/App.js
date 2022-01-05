930c8241da76eb724bb98f3a7509e41ea4c11ea2
nehachatse <66675954+nehachatse@users.noreply.github.com>
2022-01-05 20:34:08 +0530

    create components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import "./styles.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from "react-router-dom";
+
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import Search from "./Components/Search";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/home" component={Home} />
+          <Route exact path="/search" component={Search} />
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     </div>
   );
 }
